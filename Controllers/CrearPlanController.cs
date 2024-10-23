58dadc3485be8e3cd8557e3a5353d78cbe86409a
Android Studio <devandroid@ort>
2024-10-23 09:09:17 -0300

    Creating the second step of 'crear plan'

---
+++ Controllers/CrearPlanController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using JuntifyApp.Models;
+
+namespace JuntifyApp.Controllers;
+
+public class CrearPlanController : Controller
+{
+    private readonly ILogger<CrearPlanController> _logger;
+
+    public CrearPlanController(ILogger<CrearPlanController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Personalizado(){
+        return View();
+    }
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public JsonResult ObtenerLugares(){
+        string[] a ={"hola","como","te","va"};
+        return Json(new {Lista=a});
+    }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
