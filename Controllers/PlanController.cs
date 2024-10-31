095e661fb42a3269abf21aa975f167b4bc0b836f
Android Studio <devandroid@ort>
2024-10-31 13:43:47 -0300

    advances in configurar perfil

---
+++ Controllers/PlanController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using JuntifyApp.Models;
+
+namespace JuntifyApp.Controllers;
+
+public class PlanController : Controller
+{
+    private readonly ILogger<PlanController> _logger;
+
+    public PlanController(ILogger<PlanController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult VerPlan(){
+        return View();
+    }
+    public IActionResult DetalleLugar(int idLugar){
+        
+        return View();
+    }
+    
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
