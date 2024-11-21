f2bf03fe6b572dd22e409312505547b452aaad67
IVÁN KOVADLOFF <48044215@ort>
2024-11-21 15:52:10 -0300

    advances in perfil

--- Controllers/CrearPlanController.cs
+++ Controllers/CrearPlanController.cs
@@ -15,36 +15,49 @@ public class CrearPlanController : Controller
 
     public IActionResult Index()
     {
+        ViewBag.FotoUsuario=Sesion.userActual.FotoPerfil;
         ViewBag.NombreUsuario=Sesion.userActual.Nombre;
         ViewBag.logeado=Sesion.EstaLogeado;
         return View();
     }
     public IActionResult Personalizado(){
+        ViewBag.FotoUsuario=Sesion.userActual.FotoPerfil;
         ViewBag.NombreUsuario=Sesion.userActual.Nombre;
         ViewBag.logeado=Sesion.EstaLogeado;
         Sesion.IniciarCreacionPlan();
         return View();
     }
     public IActionResult AgregarParticipantes(){
+        ViewBag.FotoUsuario=Sesion.userActual.FotoPerfil;
+        ViewBag.NombreUsuario=Sesion.userActual.Nombre;
+        ViewBag.logeado=Sesion.EstaLogeado;
+        return View();
+    }
+     public IActionResult Amigos(){
+        ViewBag.FotoUsuario=Sesion.userActual.FotoPerfil;
         ViewBag.NombreUsuario=Sesion.userActual.Nombre;
         ViewBag.logeado=Sesion.EstaLogeado;
         return View();
     }
     public IActionResult FinalizarPlan(){
+        ViewBag.FotoUsuario=Sesion.userActual.FotoPerfil;
         ViewBag.NombreUsuario=Sesion.userActual.Nombre;
         ViewBag.logeado=Sesion.EstaLogeado;
         return View();
     }
     public IActionResult PlanCreado(){
+        ViewBag.FotoUsuario=Sesion.userActual.FotoPerfil;
         ViewBag.NombreUsuario=Sesion.userActual.Nombre;
         ViewBag.logeado=Sesion.EstaLogeado;
         return View("PlanCreado");
     }
     public void TomarParticipantes(string[] participantes){
+        ViewBag.FotoUsuario=Sesion.userActual.FotoPerfil;
         ViewBag.NombreUsuario=Sesion.userActual.Nombre;
     }
     [HttpGet]
     public List<Usuario> ListaAmigos(){
+        ViewBag.FotoUsuario=Sesion.userActual.FotoPerfil;
         ViewBag.NombreUsuario=Sesion.userActual.Nombre;
         Console.WriteLine(Sesion.ListarAmigos(Sesion.userActual.idUsuario));
         ViewBag.logeado=Sesion.EstaLogeado;
@@ -56,6 +69,7 @@ public class CrearPlanController : Controller
         return View();
     }
     public void InsertarTypeLugar(string type){
+        ViewBag.FotoUsuario=Sesion.userActual.FotoPerfil;
         ViewBag.NombreUsuario=Sesion.userActual.Nombre;
         Sesion.CreandoPlan.TipoLugar=type;
         Console.WriteLine(Sesion.CreandoPlan.TipoLugar);
